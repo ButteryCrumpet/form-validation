ee6f4837696a9a2f5e25e0672056654d4fe625ff
Simon Leigh <crumpetybumpety@gmail.com>
2018-12-14 18:10:41 +0900

    binary search tree for form

--- src/field.ts
+++ src/field.ts
@@ -1,19 +1,13 @@
 import { Result, Ok, Err } from "./result";
-import factory from "./factory"
-import validators from "./default-validator-conf"
-import parse from "./parser"
 
-const VALIDATION_ATTR = "data-pp-v"
-
-interface Field {
-  required: boolean
-  name: string
-  value: string | boolean
-  validator: Function
-  errors: ReadonlyArray<string>
+export interface Field {
+  readonly required: boolean
+  readonly name: string
+  readonly value: string
+  readonly errors: string[]
 }
 
-const loadedFactory = factory(validators)
+export type Validation<T> = (field: string) => Result<Field, T>
 
 type fromElement = (ele: HTMLInputElement) => Result<Field, string>
 export const fromElement: fromElement
@@ -21,21 +15,23 @@ export const fromElement: fromElement
     if (!ele.hasAttribute("name")) {
       return Err("Must have a name attribute")
     }
-    const validation = ele.getAttribute(VALIDATION_ATTR) || ""
     const field = {
       name: ele.getAttribute("name") || "",
-      required: validation.lastIndexOf("required") !== -1 || ele.required,
+      required: ele.required,
       value: ele.value,
-      validator: loadedFactory(parse(validation)),
       errors: []
     }
     return Ok(field)
   }
 
 type update = (field: Field) => (value: string) => Field
-export const validate: update
-  = field => value => ({...field, value: value, errors: field.validator(value)})
+export const update: update
+  = field => value => ({...field, value: value})
 
 type isValid = (field: Field) => boolean
 export const isValid: isValid
   = field => field.errors.length === 0
+
+type validate = <T>(validation: Validation<T>) => (field: Field) => Result<Field, T>
+export const validate: validate
+ = validation => field => validation(field.value)
--- src/result.ts
+++ src/result.ts
@@ -1,40 +1,34 @@
 
 export type Result<T, E> = Ok<T> | Err<E>
 
-interface Ok<T> {
-  type: "ok",
-  value: T
-}
+type Ok<T> = ["ok", T]
 
-interface Err<E> {
-  type: "err",
-  value: E
-}
+type Err<E> = ["err", E]
 
 type makeErr = <T>(message: T) => Err<T>
 export const Err: makeErr
-  = message => ({ type: "err", value: message })
+  = message => ["err", message]
 
 type makeOk = <T>(value: T) => Ok<T>
 export const Ok: makeOk
- = value => ({ type: "ok", value: value })
+ = value => ["ok", value]
 
 export function isOk<T, E>(result: Result<T, E>): result is Ok<T> {
-  return result.type === "ok"
+  return result[0] === "ok"
 }
 
 export function isErr<T, E>(result: Result<T, E>): result is Err<E> {
-  return result.type === "err"
+  return result[0] === "err"
 }
 
 type map = <U, T, E>(fn: (val: T) => U) => (result: Result<T, E>) => Result<U, E>
 export const map: map
-  = fn => result => isOk(result) ? Ok(fn(result.value)) : result 
+  = fn => result => isOk(result) ? Ok(fn(result[1])) : result 
     
 type mapErr = <U, T, E>(fn: (val: E) => U) => (result: Result<T, E>) => Result<T, U>
 export const mapErr: mapErr
-  = fn => result => isErr(result) ? Err(fn(result.value)) : result 
+  = fn => result => isErr(result) ? Err(fn(result[1])) : result 
 
 type andThen = <U, T, E>(fn: (val: T) => Result<U, E>) => (result: Result<T, E>) => Result<U, E>
 export const andThen: andThen
-  = fn => result => isOk(result) ? fn(result.value) : result
+  = fn => result => isOk(result) ? fn(result[1]) : result
