2a7290311f4c375edc69ce461599e38b729c3234
Simon Leigh <crumpetybumpety@gmail.com>
2019-04-19 11:43:54 +0900

    prettier formatting edits

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,32 @@ import { reducer } from "./reducer";
 import { render } from "./render";
 import { inputToField } from "./field-builders";
 
-
 type Configuration = {
-  form?: HTMLFormElement
-  formAttr?: string
-  vAttr?: string
-  names?: {[key: string]: string}
-  messages?: {[key: string]: string}
-  debug?: boolean
+  form?: HTMLFormElement;
+  formAttr?: string;
+  vAttr?: string;
+  names?: { [key: string]: string };
+  messages?: { [key: string]: string };
+  debug?: boolean;
 };
 
 const defaultConfig: Configuration = {
   messages: {
     required: "{name}を入力してください",
-    default: "{name}は無効です",
+    default: "{name}は無効です"
   },
   formAttr: "data-pp-form",
   vAttr: "data-v",
-  debug: false,
+  debug: false
 };
 
 export function init(config: Configuration) {
-
   config = { ...defaultConfig, ...config };
 
   const form = config.form
     ? config.form
-    : document.querySelector(`[${config.formAttr}]`) as HTMLFormElement;
-  
+    : (document.querySelector(`[${config.formAttr}]`) as HTMLFormElement);
+
   if (!form) {
     throw new Error(`No form with attribute ${config.formAttr}`);
   }
@@ -41,45 +39,61 @@ export function init(config: Configuration) {
   );
 
   const _render = render(config.names || {}, config.messages || {});
-  store.subscribe(() => _render(store.getState()));
-  
-  const _updateEvent = updateEvent(store, config.vAttr || "");
-
-  form.addEventListener("change", _updateEvent);
-
-  form.addEventListener("invalid", (e) => {
-    e.preventDefault();
-    _updateEvent(e);
-  }, true);
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    store.dispatch({type: "submit"});
+  store.subscribe(() => {
+    if (config.debug) {
+      console.debug("Render start");
+    }
+    _render(store.getState());
   });
 
-  store.dispatch({type: "init", payload: { ele: form, attr: config.vAttr }});
+  const _updateEvent = updateEvent(store, config.vAttr || "");
+
+  form.addEventListener("change", e => {
+    if (config.debug) {
+      console.debug("Native change event");
+    }
+    _updateEvent(e);
+  });
+
+  form.addEventListener(
+    "invalid",
+    e => {
+      if (config.debug) {
+        console.debug("Native invalid event");
+      }
+      e.preventDefault();
+      _updateEvent(e);
+    },
+    true
+  );
+
+  form.addEventListener("submit", e => {
+    if (config.debug) {
+      console.debug("Native submit event");
+    }
+    e.preventDefault();
+    store.dispatch({ type: "submit" });
+  });
+
+  store.dispatch({ type: "init", payload: { ele: form, attr: config.vAttr } });
 
   return (action: Redux.AnyAction) => store.dispatch(action);
 }
 
-
 type updateEvent = (store: Redux.Store, attr: string) => (e: Event) => void;
-const updateEvent: updateEvent = (store, attr) => e =>
-  {
-    const target = e.target as HTMLInputElement;
-    store.dispatch({
-      type: "update",
-      payload: inputToField(target, attr)
-    });
-  };
-
+const updateEvent: updateEvent = (store, attr) => e => {
+  const target = e.target as HTMLInputElement;
+  store.dispatch({
+    type: "update",
+    payload: inputToField(target, attr)
+  });
+};
 
 // Middleware
 
 const logger: Redux.Middleware = store => next => action => {
-  console.log("dispatching", action);
+  console.debug("dispatching", action);
   const result = next(action);
-  console.log('next state', store.getState());
+  console.debug("next state", store.getState());
   return result;
 };
-
--- src/reducer.ts
+++ src/reducer.ts
@@ -1,9 +1,18 @@
 import { Reducer } from "redux";
-import { Form, ChangeSet, update, form, validate, valid, Insertable, insert, remove } from "./form/form";
+import {
+  Form,
+  ChangeSet,
+  update,
+  form,
+  validate,
+  valid,
+  Insertable,
+  insert,
+  remove
+} from "./form/form";
 import { validationFactory } from "./validation";
 import { FormActions, Config, ActionTypes } from "./actions";
 import { inputToField } from "./field-builders";
-
 
 export interface State {
   readonly formElement: HTMLFormElement;
@@ -14,7 +23,6 @@ export interface State {
 const _validate = validate(memoise(validationFactory));
 
 export const reducer: Reducer = (state: State, action: FormActions) => {
-
   switch (action.type) {
     case ActionTypes.INIT:
       return init(action.payload);
@@ -29,70 +37,52 @@ export const reducer: Reducer = (state: State, action: FormActions) => {
     default:
       return state;
   }
-
 };
-
-
 
 // ACTION REDUCERS
 
 type init = (config: Config) => State;
-const init: init = config => 
-  {
-    const elements = config.ele.querySelectorAll(`[${config.attr}]`);
-    const inputs = Array.prototype.slice.call(elements);
-    
-    return {
-      formElement: config.ele,
-      form: form(inputs.map(e => inputToField(e, config.attr))),
-      attr: config.attr
-    };
+const init: init = config => {
+  const elements = config.ele.querySelectorAll(`[${config.attr}]`);
+  const inputs = Array.prototype.slice.call(elements);
+
+  return {
+    formElement: config.ele,
+    form: form(inputs.map(e => inputToField(e, config.attr))),
+    attr: config.attr
   };
-
-
+};
 
 type updateInput = (state: State, change: ChangeSet<string>) => State;
-const updateInput: updateInput = (state, change) =>
-  ({
-    ...state,
-    form: _validate(update(change, state.form))
-  });
-
-
+const updateInput: updateInput = (state, change) => ({
+  ...state,
+  form: _validate(update(change, state.form))
+});
 
 type addInput = (state: State, element: HTMLInputElement) => State;
-const addInput: addInput = (state, element) =>
-  ({
-    ...state,
-    form: insert(inputToField(element, state.attr))(state.form)
-  });
-
-
+const addInput: addInput = (state, element) => ({
+  ...state,
+  form: insert(inputToField(element, state.attr))(state.form)
+});
 
 type removeInput = (state: State, name: string) => State;
-const removeInput: removeInput = (state, name) =>
-  ({
-    ...state,
-    form: remove(name)(state.form)
-  });
-
-
+const removeInput: removeInput = (state, name) => ({
+  ...state,
+  form: remove(name)(state.form)
+});
 
 type submitForm = (state: State) => State;
-const submitForm: submitForm = state =>
-  {
-    const form = _validate(state.form, true);
-    if (valid(form)) {
-      state.formElement.submit();
-    }
-    return { ...state, form };
-  };
-
-
+const submitForm: submitForm = state => {
+  const form = _validate(state.form, true);
+  if (valid(form)) {
+    state.formElement.submit();
+  }
+  return { ...state, form };
+};
 
 // HELPERS
 
-type memo<T> = {[key: string]: T};
+type memo<T> = { [key: string]: T };
 function memoise<T>(fn: (str: string, r: boolean) => T) {
   const memo: memo<T> = {};
   return (str: string, r: boolean) => {
